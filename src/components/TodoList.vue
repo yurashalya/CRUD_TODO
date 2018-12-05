1f6d5e7e9ffe01d1876b6f76d82c49737039d6ed
Yura Shalya <stallker9696@gmail.com>
2018-12-06 01:24:33 +0200

    Creating properties to cancel an action

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -5,16 +5,24 @@
         v-model="newTodo"
         @keyup.enter="addTodo"
         >
-        <div v-for="(todo, index) in todos" 
-        :key="todo.id" 
-        class="todo-item"> 
-            <div> {{ todo.title }} </div>
-            <div class="remove-item"
-            @click="removeTodo(index)"
-            > &times; </div>
+        <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item"> 
+            <div class="todo-item-left"> 
+                <div v-if="!todo.editing" @dblclick="editTodo(todo)" 
+                class="todo-item-label"> {{ todo.title }} </div>
+                <input v-else class="todo-item-edit" type="text" 
+                v-model="todo.title"
+                @blur="doneEdit(todo)"
+                @keyup.enter="doneEdit(todo)"
+                @keyup.esc="cancelEdit(todo)"
+                v-focus
+                >
+            </div>
+            <div class="remove-item" @click="removeTodo(index)">
+                 &times; 
+            </div>
 
         </div>
-
+ 
 
        
     </div>
@@ -27,26 +35,38 @@ export default {
     return {
       newTodo: '',
       idForTodo: 3,
+      beforeEditCache: ' ',
       todos: [
         {
         'id': 1,
         'title': 'Finish Vue',
         'completed': false,
+        'editing': false
         },
         {
         'id': 2,
         'title': 'Take over world',
         'completed': false,
+        'editing': false
         },
       ]
     }
   },
+
+  directives: {
+    focus: {
+      inserted: function (el) {
+        el.focus()
+      }
+    }
+  },
+   
   methods: {
-      addTodo() {
+    addTodo() {
+
         if(this.newTodo.trim().length == 0) {
             return
         }
-
 
         this.todos.push({
             id: this.idForTodo,
@@ -56,10 +76,29 @@ export default {
 
         this.newTodo = '',
         this.idForTodo++
-      },
-      removeTodo(index) {
-          this.todos.splice(index, 1)
-      }
+    },
+
+    editTodo(todo) {
+        this.beforeEditCache = todo.title
+        todo.editing = true
+    }, 
+
+    doneEdit(todo) {
+        if(todo.title.trim() == '') {
+            todo.title = this.beforeEditCache
+        }
+
+        todo.editing = false
+    },
+
+    cancelEdit(todo) {
+        todo.this = this.beforeEditCache
+        todo.editing = false
+    },
+
+    removeTodo(index) {
+        this.todos.splice(index, 1)
+    },
   }
 }
 </script>
@@ -92,6 +131,31 @@ export default {
 
     &:hover {
       color: black;
+    }
+  }
+
+  .todo-item-left { 
+    display: flex;
+    align-items: center;
+  }
+
+  .todo-item-label {
+    padding: 10px;
+    border: 1px solid white;
+    margin-left: 12px;
+  }
+
+  .todo-item-edit {
+    font-size: 24px;
+    color: #2c3e50;
+    margin-left: 12px;
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+
+    &:focus {
+      outline: none;
     }
   }
 </style>
